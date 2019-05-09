4e87d6caaae3535057f717c5a84fa59f047ee5c0
Vakhrushev Kirill <vakhrushev.kirill93@yandex.ru>
2019-05-09 20:14:41 +0300

    Requsets install

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import requsts
 
 app = Flask(__name__)
 
@@ -6,7 +7,9 @@ app = Flask(__name__)
 @app.route('/', methods=['POST', 'GET'])
 def main():
     if request.method == 'POST':
+        requsts.
         print(request.form['text_request'])
+    
     return render_template('base.html')
 
 
